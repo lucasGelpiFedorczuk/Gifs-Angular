9e5a1ea36ecac3fa3cc4f89fa1c178a681ff438b
lucasGelpiFedorczuk <lucasglp32@gmail.com>
2025-11-23 15:53:27 -0300

    Diseño página de busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -29,7 +29,20 @@ export class GifService {
             const gifs = GifMapper.mapGiphyItemstoGifArray(resp.data);
             this.trendingGifs.set(gifs);
             this.trendingGifsLoading.set(false);
-            console.log({gifs});
+        } )
+    }
+
+    searchGifs(query: string) {
+        this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/search`, {
+            params: {
+                api_key: environment.giphyApiKey,
+                limit: 20,
+                q: query,
+            }
+        })
+        .subscribe( (resp) => {
+            const gifs = GifMapper.mapGiphyItemstoGifArray(resp.data);
+            console.log({search: gifs});
         } )
     }
 }
